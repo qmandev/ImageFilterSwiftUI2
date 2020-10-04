9505faf6073ba1b20d9f606c518b56fcc473a9af
qmandev <qiangma7@gmail.com>
2020-10-04 18:24:56 -0400

    Initial Commit

---
+++ ImageFilterSwiftUI2/ImageFilterSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFilterSwiftUI2App.swift
+//  ImageFilterSwiftUI2
+//
+//  Created by Qiang Ma on 10/4/20.
+//
+
+import SwiftUI
+
+@main
+struct ImageFilterSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
